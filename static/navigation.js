cd774cf8753c4dffee52d649632f87019a359f6d
StoreOnce <noreply@hpe.com>
2018-11-05 14:16:07 +0000

    Initial commit

---
+++ static/navigation.js
@@ -0,0 +1,166 @@
+/* Find the json file names in the /json directory. */
+$(document).ready(function () {
+  $("#metadata").hide();
+  $(function () {
+    $("#landing-desc").load("static/landing-info.html");
+  });
+
+  $.getJSON('versions.json', function (item) {
+    $.each(item, function (key, val) {
+      var isLastIteration = key == item.length - 1;
+      var apiVersion = val.name;
+
+      // Add button for each API version
+      var buttons = $(
+        "<a id=" + apiVersion + ">" + apiVersion + "</a>"
+      ).addClass("apiVersion");
+      buttons.appendTo("#myDropdown");
+
+      document.body.addEventListener("click", function (event) {
+        if (event.srcElement.id == apiVersion) {
+          getAPI(apiVersion);
+        }
+      });
+
+      // Update latest version on landing page
+      if (isLastIteration) {
+        $("#apiVersion").text(apiVersion);
+        getAPI(apiVersion);
+      }
+    });
+  });
+});
+
+/* Takes the API version and displays contents of that repo */
+function getAPI(apiVersion) {
+  // Update title
+  $("#selectedAPI").text(apiVersion);
+  // Remove current items
+  $(".ui-menu-item").remove();
+  // Update for current API
+  $.getJSON('./versions/' + apiVersion + '/specifications.json', function (item) {
+    $.each(item, function (key, val) {
+      $.getJSON(val.path, function (json) {
+        var jsonTitle = json.info.title;
+        // Using local storage to allow for file changing (x-isPrivate)
+        localStorage.setItem(jsonTitle, JSON.stringify(json));
+        var stringJSON = localStorage.getItem(jsonTitle);
+        var parsedJSON = removeIsPrivate(JSON.parse(stringJSON));
+
+        // Create list elements
+        var navList = $("ul.navList");
+        var li = $("<li/>")
+          .addClass("ui-menu-item")
+          .attr("role", "menuitem")
+          .appendTo(navList);
+        // Creating <a> links to each json file
+        jQuery("<a/>", {
+          href: jsonTitle,
+          class: "ui-all",
+          id: val.name,
+          text: jsonTitle
+        }).appendTo(li);
+        // Adding an event listener to add link to new page.
+        document.body.addEventListener("click", function (event) {
+          if (event.srcElement.id == val.name) {
+            event.preventDefault();
+            loadPage(parsedJSON);
+          }
+        });
+      });
+    });
+  });
+}
+
+/* Sort list as each file is loaded */
+$(document).ajaxComplete(function () {
+  sortList("list");
+});
+
+/* Sorts HTML list elements */
+function sortList(ul) {
+  var ul = document.getElementById(ul);
+  Array.from(ul.getElementsByTagName("LI"))
+    .sort((a, b) => a.textContent.localeCompare(b.textContent))
+    .forEach(li => ul.appendChild(li));
+}
+
+/* Uses SwaggerUIBundle to render the JSON into HTML */
+function loadPage(url) {
+  $("#landing-desc").hide();
+  const ui = SwaggerUIBundle({
+    spec: url,
+    dom_id: "#swagger-ui",
+    presets: [
+      SwaggerUIBundle.presets.apis,
+      // yay ES6 modules ↘
+      Array.isArray(SwaggerUIHpeViewitPreset)
+        ? SwaggerUIHpeViewitPreset
+        : SwaggerUIHpeViewitPreset.default
+    ],
+    plugins: [SwaggerUIBundle.plugins.DownloadUrl],
+    layout: "HpeViewitLayout"
+  });
+
+  window.ui = ui;
+  clearSearch();
+}
+
+/* API version dropdown */
+function dropdown() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+/* Close the dropdown */
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+/* Searches <a> tags inside of nav bar */
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("sidebar-content");
+  a = div.getElementsByTagName("a");
+  for (i = 0; i < a.length; i++) {
+    if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
+
+/* Clears the search bar */
+function clearSearch() {
+  document.getElementById("myInput").value = "";
+  filterFunction();
+}
+
+function goHome() {
+  location.reload();
+}
+
+/* Hiding any endpoints with "x-isPrivate" flag */
+function removeIsPrivate(specification) {
+  for (path in specification.paths) {
+    for (method in specification.paths[path]) {
+      if (specification.paths[path][method]["x-isPrivate"] == "true") {
+        delete specification.paths[path][method];
+        break;
+      }
+    }
+    delete specification.info.version;
+    return specification;
+  }
+}
